0b11a53459b00f553483588f92ec306a68d9b63c
Bajric Nedim <76698338+nedim-bajric@users.noreply.github.com>
2023-01-22 15:56:35 +0100

    rmv pgs2

--- site/lib/search-props.tsx
+++ site/lib/search-props.tsx
@@ -10,11 +10,11 @@ export async function getSearchStaticProps({
   const config = { locale, locales }
   const pagesPromise = commerce.getAllPages({ config, preview })
   const siteInfoPromise = commerce.getSiteInfo({ config, preview })
-  const { pages } = await pagesPromise
+  // const { pages } = await pagesPromise
   const { categories, brands } = await siteInfoPromise
   return {
     props: {
-      pages,
+
       categories,
       brands,
     },
